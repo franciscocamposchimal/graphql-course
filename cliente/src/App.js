a8269305053c375011ddc6902255a4c8fcc5c4fb
franciscocamposchimal <camposfranciscoing@gmail.com>
2020-01-15 17:45:34 -0600

    config

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { ApolloProvider } from 'react-apollo';
 import ApolloClient from 'apollo-boost';
 // Material UI
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
+import { 
+  makeStyles, 
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton
+   } from '@material-ui/core';
+// Icons
 import MenuIcon from '@material-ui/icons/Menu';
 
 const client = new ApolloClient({
