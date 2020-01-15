f6b46657da0d4a00935d7f8e8b5948dfbc6ed5f6
franciscocamposchimal <camposfranciscoing@gmail.com>
2020-01-14 23:07:37 -0600

    client/material_ui

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { ApolloProvider } from 'react-apollo';
 import ApolloClient from 'apollo-boost';
+// Material UI
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const client = new ApolloClient({
   uri: "http://localhost:8000/graphql",
@@ -10,13 +17,32 @@ const client = new ApolloClient({
   }
 });
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+}));
+
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
       <ApolloProvider client={client}>
-        <h1>
-          Holii!!
-        </h1>
+      <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit">
+            CERVUS CRM
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
       </ApolloProvider>
     </div>
   );
