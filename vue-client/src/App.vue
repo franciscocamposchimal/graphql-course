b419e5b53d986ecbdf3a13dafbb7050297409bd6
Francisco <francisco@cervusconsulting.com>
2020-01-21 18:05:03 -0600

    front/vue client

---
+++ vue-client/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+      </div>
+    </v-app-bar>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  components: {
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
---
+++ vue-client/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <v-layout wrap>
+      <v-flex xs12>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+
+  data: () => ({
+    
+  })
+};
+</script>
